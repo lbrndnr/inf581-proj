5cd8688e79a8867a815266f6f1dfff8e7df1f559
ICEtinger <12306216+ICEtinger@users.noreply.github.com>
2018-02-14 17:12:03 +0100

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,6 +1,9 @@
 from queue import *
 import random
 
+def test():
+    print("AAAA")
+
 class environment:
     
     # mov[direction] is the displacement for the snake's head.
@@ -11,56 +14,56 @@ class environment:
     Instead of a single mouse, there may be multiple mice. Their points are given by mice_points list.
     """
     def __init__(self, maze_size=20, initial_snake_size=5, mice_points=[1,2,3], fixed_obstacles=[(3,4),(3,5),(3,6)]):
-        score = 0
-        snake_q = Queue()
+        self.score = 0
+        self.snake_q = Queue()
         # obstacles are walls + fixed obstacles + the snake itself
-        obstacles = set()
+        self.obstacles = set()
         for obs in fixed_obstacles:
-            obstacles.add(obs)
+            self.obstacles.add(obs)
         # maze walls
         for i in range (maze_size):
-            obstacles.add( (-1       ,i        ) );
-            obstacles.add( (maze_size,i        ) );
-            obstacles.add( (i        ,-1       ) );
-            obstacles.add( (i        ,maze_size) );
+            self.obstacles.add( (-1       ,i        ) );
+            self.obstacles.add( (maze_size,i        ) );
+            self.obstacles.add( (i        ,-1       ) );
+            self.obstacles.add( (i        ,maze_size) );
         # snake
         for i in range (initial_snake_size):
-            obstacles.add( (0,i) )
-            snake_q.put( (0,i) )
-        head = (0,initial_snake_size-1)
-        curr_direction = 0
+            self.obstacles.add( (0,i) )
+            self.snake_q.put( (0,i) )
+        self.head = (0,initial_snake_size-1)
+        self.curr_direction = 0
         # mice = dict{ key=coordinate, value=points }
-        mice = {}
+        self.mice = {}
         for i in mice_points:
             # takes care to not place a mouse on the snake or over another mouse
             coord = (0,0)
-            while (coord in mice or coord in self.obstacles):
+            while (coord in self.mice or coord in self.obstacles):
                 coord = (random.randint(0,maze_size), random.randint(0,maze_size))
-            mice[coord] = i
+            self.mice[coord] = i
     
     # returns true if the game has ended. False othersie.
-    def move(direction):
+    def move(self, direction):
         # remove tail
-        obstacles.remove(snake_q.get())
-        self.head = (head[0]+mov[direction][0], head[1]+mov[direction][1])
+        self.obstacles.remove(self.snake_q.get())
+        self.head = (self.head[0]+mov[direction][0], self.head[1]+mov[direction][1])
         # checks if snake hits an obstacle
-        if head in obstacles:
+        if self.head in self.obstacles:
             return true
         # adds new position
-        obstacles.add(head)
-        snake_q.add(head)
-        curr_direction = direction
+        self.obstacles.add(self.head)
+        self.snake_q.add(self.head)
+        self.curr_direction = direction
         # checks if ate a mouse
-        if head in mice:
-            points = mice[head]
-            score += points
+        if self.head in self.mice:
+            points = self.mice[head]
+            self.score += points
             # replace eaten mouse in a random position (without another mouse or the snake).
-            mice.remove[head]
-            coord = head
-            while (coord in mice or coord in obstacles):
+            self.mice.remove[self.head]
+            coord = self.head
+            while (coord in self.mice or coord in self.obstacles):
                 coord = (random.randint(0,maze_size), random.randint(0,maze_size))
-            mice[coord] = points 
+            self.mice[coord] = points 
         return false
         
-    def see_maze():
-        return obstacles, mice
+    def see_maze(self):
+        return self.obstacles, self.mice
