e9742284eabf32fc55d2482a4b93aed7158313c8
ICEtinger <12306216+ICEtinger@users.noreply.github.com>
2018-02-14 17:44:24 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,8 +1,39 @@
 from environment import *
 
+# mov[direction] is the displacement for the snake's head.
+mov = [(0,1), (0,-1), (1,0), (-1,0)]
+    
+# the size of the maze region accessible from the snake's head
+# (i.e.: that the snake can go to without hitting an obstacle).
+def size_of_accessible_region(state):
+    def DFS(tmp_obstacles, coord):
+        if coord in tmp_obstacles:
+            return 0
+        size = 1
+        # mark as visited
+        tmp_obstacles.add(coord)
+        # for each direction, visit adjacent cell
+        for dir in range(4):
+            size += DFS( tmp_obstacles, (coord[0]+mov[dir][0],coord[1]+mov[dir][1]) )
+        return size
+    
+    obstacles, mice, head, curr_direction = state        
+    tmp = obstacles.copy()
+    tmp.remove(head)
+    return DFS(tmp, head)
+
 env = environment()
 
+
+
+
+
 # just to test
-b,c = a.see_maze()
-print(b)
-print(c)
+state = env.see_maze()
+obstacles, mice, head, curr_direction = state
+#print(obstacles)
+print(mice)
+print(head)
+print(curr_direction)
+
+print(size_of_accessible_region(state))
