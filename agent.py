57e4bd406257aa2a3fcbf28e4de06d4a8411ba22
ICEtinger <12306216+ICEtinger@users.noreply.github.com>
2018-02-27 18:19:32 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from environment import environment
+import copy
 
 # mov[direction] is the displacement for the snake's head.
 mov = [(0,1), (0,-1), (1,0), (-1,0)]
@@ -37,23 +38,29 @@ def power_list(list_, power):
     else:
         return [power_list(list_, power - 1) for i in list_]
 
-def apply_for_each_on_power_list(power_list, function_to_apply):
-    for i,list_ in enumerate()
-
-
+def get_rewards_in_power_list(env_to_copy, power_list):
+    if type(power_list) is list:    
+        to_return = []
+        for i,inner_pl in enumerate(power_list):
+            tmp_environmnet = copy.deepcopy(env_to_copy)
+            tmp_environmnet.move(i)
+            to_return.append(get_rewards_in_power_list(tmp_environmnet, inner_pl))
+        return to_return
+    else:
+       return reward(env_to_copy.get_state()) 
 
 env = environment()
 hit_an_obstacle = False
 depth = 3 ######### parameter to change ##########
 while hit_an_obstacle == False:
-    rewards = power_list([0,0,0,0], depth)
-  
-    
+    pl = power_list([0,0,0,0], depth)
+    rewards = get_rewards_in_power_list(env, pl)
+    # to do
     
     
 
 # just to test
-state = env.see_maze()
+state = env.get_state()
 obstacles, mice, head, curr_direction, score, game_terminated = state
 #print(obstacles)
 print(mice)
