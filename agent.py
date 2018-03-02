99b90413435d13914f163f232296479d51c9060f
Laurin Brandner <mail@laurinbrandner.ch>
2018-03-02 14:39:07 +0100

    Add qlearning

--- agent.py
+++ agent.py
@@ -1,85 +1,138 @@
-from environment import environment
-import copy
 import numpy as np
-from math import sqrt
+from environment import *
+from geometry import *
 
-# mov[direction] is the displacement for the snake's head.
-mov = [(0,1), (0,-1), (1,0), (-1,0)]
+
+def compute_direction(a, b):
+    diff = b - a
+    if abs(diff[0]) > abs(diff[1]):
+        if diff[0] > 0:
+            return 0 #up
+        else:
+            return 1 #down
+    else:
+        if diff[1] > 0:
+            return 2 #right
+        else:
+            return 3 #left
+
+
+def decide(env, state):
+    maze, head, tail, direction = state
+    maze_size = maze.shape[0]
     
-# the size of the maze region accessible from the snake's head
-# (i.e.: that the snake can go to without hitting an obstacle).
-def size_of_accessible_region(state):
-    def DFS(tmp_obstacles, coord):
-        if coord in tmp_obstacles:
-            return 0
-        size = 1
-        # mark as visited
-        tmp_obstacles.add(coord)
-        # for each direction, visit adjacent cell
-        for dir in range(4):
-            size += DFS( tmp_obstacles, (coord[0]+mov[dir][0],coord[1]+mov[dir][1]) )
-        return size
+    ds = [head + vectorize(direction, a) for a in actions]
+    ds = [1 if env.check_field(p) >= 0 else 0 for p in ds]
+    blocked_actions = int("".join(str(e) for e in ds), 2)
     
-    obstacles, mice, head, curr_direction, score, game_terminated = state
-    tmp = obstacles.copy()
-    tmp.remove(head)
-    return DFS(tmp, head)
+    mice = np.where(maze > 0)
+    mice = np.transpose(mice)
+    mice_directions = [compute_direction(head, m) for m in mice]
 
-def reward(state):
-    obstacles, mice, head, curr_direction, score, game_terminated = state
-    proximity_to_mice = 0
-    for coord,points in mice.items():
-        dist_to_mouse = sqrt((head[0]-coord[0])**2 + (head[1]-coord[1])**2)
-        if dist_to_mouse != 0:
-            proximity_to_mice += points / dist_to_mouse
-    if game_terminated:
-        return 10 * score - 100100100
-    else:
-        return 10 * score + size_of_accessible_region(state) + proximity_to_mice
+    return (mice_directions[0], blocked_actions)
+
+    return tuple(mice_directions + [blocked_actions])
 
 
-def power_list(list_, power):
-    if power == 1:
-        return list_
-    else:
-        return [power_list(list_, power - 1) for i in list_]
+def train_MC():
+    epochs = 500000 # Number of episodes/plays
+    epsilon = 1. # E-greedy
 
-def get_rewards_in_power_list(env_to_copy, power_list_):
-    if type(power_list_) is list:    
-        to_return = []
-        for i,inner_pl in enumerate(power_list_):
-            tmp_environment = copy.deepcopy(env_to_copy)
-            tmp_environment.move(i)
-            to_return.append(get_rewards_in_power_list(tmp_environment, inner_pl))
-        return to_return
-    else:
-       return reward(env_to_copy.get_state()) 
+    maze_size = 10
+    n_c = 4 # direction of the mice
+    n_a = len(actions) #possible actions
+    n_d = 2**n_a #directions the snake can take without hitting an obstacle
 
-def get_max_from_power_list(power_list_):
-    if type(power_list_) is list:
-        return max([get_max_from_power_list(inner_pl) for inner_pl in power_list_])
-    else:
-        return power_list_     
+    dim = tuple(1*[n_c] + [n_d, n_a])
+    qv = np.zeros(dim) #This creates our Q-value look-up table
+    sa_count = np.zeros(dim) #Record how many times we've seen a given state-action pair.
+    returnSum = 0
+    stepSum = 0
 
-env = environment()
-hit_an_obstacle = False
-depth = 3 ######### parameter to change ##########
-while hit_an_obstacle == False:
-    pl = power_list([0,0,0,0], depth)
-    rewards = get_rewards_in_power_list(env, pl)
-    optimal_direction = np.argmax([get_max_from_power_list(rewards[i]) for i in range(4)])        
-    env.move(optimal_direction)
-    obstacles, mice, head, curr_direction, score, game_terminated = env.get_state()
-    hit_an_obstacle = game_terminated
-    ##### IT WORKS! JUST RUN IT! #####
-    env.print_maze()
+    for i in range(epochs):
+        env = environment(maze_size=maze_size)
+        state = env.state
+        ended = False
+        episodeReward = 0
+    
+        ds = []
+        
+        while not ended:
+            d = decide(env, state)
 
-# just to test
-state = env.get_state()
-obstacles, mice, head, curr_direction, score, game_terminated = state
-#print(obstacles)
-print(mice)
-print(head)
-print(curr_direction)
+            # E-greedy policy
+            if (np.random.random() < epsilon or np.count_nonzero(qv[d[0], d[1],:]) == 0):
+                act = np.random.randint(0, n_a)
+            else:
+                act = np.argmax(qv[d[0], d[1],:]) #select the best action
 
-print(size_of_accessible_region(state))
+            d = tuple(list(d) + [act]) #append the chosen action to the decision
+
+            sa_count[d] +=1
+            ds.append(d)
+            
+            state, reward, ended = env.step(act)
+            episodeReward += reward
+        
+        epsilon = epsilon*0.9999
+
+        # Update Q values of the visited states
+        for d in ds:
+            qv[d] = qv[d]+ (1./sa_count[d])*(episodeReward - qv[d])
+
+        returnSum += episodeReward
+        stepSum += len(ds)
+        if (i % 100 == 0 and i > 0):
+            print("Episode: ", i, "Average Return: ", returnSum/100.0, "Average Steps: ", stepSum/100.0)
+            returnSum = 0
+            stepSum = 0
+
+
+def train_QL():
+    epochs = 500000 # Number of episodes/plays
+    epsilon = 1. # E-greedy
+    gamma = 1.0
+    alpha = 0.1
+
+    maze_size = 10
+    n_c = 4 # direction of the mice
+    n_a = len(actions) #possible actions
+    n_d = 2**n_a #directions the snake can take without hitting an obstacle
+
+    dim = tuple(1*[n_c] + [n_d, n_a])
+    qv = np.zeros(dim) #This creates our Q-value look-up table
+    returnSum = 0
+    stepSum = 0
+
+    for i in range(epochs):
+        env = environment(maze_size=maze_size)
+        state = env.state
+        ended = False
+        
+        while not ended:
+            d = decide(env, state)
+
+            # E-greedy policy
+            if (np.random.random() < epsilon or np.count_nonzero(qv[d[0], d[1],:]) == 0):
+                act = np.random.randint(0, n_a)
+            else:
+                act = np.argmax(qv[d[0], d[1],:]) #select the best action
+
+            d = tuple(list(d) + [act]) #append the chosen action to the decision
+            
+            state_new, reward, ended = env.step(act)
+
+            q_next = 0 if ended else np.max(qv[d[0], d[1],:])
+            qv[d] += alpha*(reward + gamma*q_next  -  qv[d])
+            state = state_new
+            returnSum += reward
+            stepSum += 1  
+
+        epsilon = epsilon*0.9999
+        if (i % 100 == 0 and i > 0):
+            print("Episode: ", i, "Average Return: ", returnSum/100.0, "Average Steps: ", stepSum/100.0)
+            returnSum = 0
+            stepSum = 0
+
+
+train_QL()
