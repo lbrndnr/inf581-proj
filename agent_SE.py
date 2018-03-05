5e322504905307219055d3d5a61d3087c1ce9b78
ICEtinger <12306216+ICEtinger@users.noreply.github.com>
2018-03-05 18:07:19 +0100

    Add files via upload

---
+++ agent_SE.py
@@ -0,0 +1,85 @@
+from environment import environment
+import copy
+import numpy as np
+from math import sqrt
+
+# mov[direction] is the displacement for the snake's head.
+mov = [(0,1), (0,-1), (1,0), (-1,0)]
+    
+# the size of the maze region accessible from the snake's head
+# (i.e.: that the snake can go to without hitting an obstacle).
+def size_of_accessible_region(state):
+    def DFS(tmp_obstacles, coord):
+        if coord in tmp_obstacles:
+            return 0
+        size = 1
+        # mark as visited
+        tmp_obstacles.add(coord)
+        # for each direction, visit adjacent cell
+        for dir in range(4):
+            size += DFS( tmp_obstacles, (coord[0]+mov[dir][0],coord[1]+mov[dir][1]) )
+        return size
+    
+    obstacles, mice, head, curr_direction, score, game_terminated = state
+    tmp = obstacles.copy()
+    tmp.remove(head)
+    return DFS(tmp, head)
+
+def reward(state):
+    obstacles, mice, head, curr_direction, score, game_terminated = state
+    proximity_to_mice = 0
+    for coord,points in mice.items():
+        dist_to_mouse = sqrt((head[0]-coord[0])**2 + (head[1]-coord[1])**2)
+        if dist_to_mouse != 0:
+            proximity_to_mice += points / dist_to_mouse
+    if game_terminated:
+        return 10 * score - 100100100
+    else:
+        return 10 * score + size_of_accessible_region(state) + proximity_to_mice
+
+
+def power_list(list_, power):
+    if power == 1:
+        return list_
+    else:
+        return [power_list(list_, power - 1) for i in list_]
+
+def get_rewards_in_power_list(env_to_copy, power_list_):
+    if type(power_list_) is list:    
+        to_return = []
+        for i,inner_pl in enumerate(power_list_):
+            tmp_environment = copy.deepcopy(env_to_copy)
+            tmp_environment.move(i)
+            to_return.append(get_rewards_in_power_list(tmp_environment, inner_pl))
+        return to_return
+    else:
+       return reward(env_to_copy.get_state()) 
+
+def get_max_from_power_list(power_list_):
+    if type(power_list_) is list:
+        return max([get_max_from_power_list(inner_pl) for inner_pl in power_list_])
+    else:
+        return power_list_     
+
+env = environment()
+hit_an_obstacle = False
+depth = 3 ######### parameter to change ##########
+while hit_an_obstacle == False:
+    pl = power_list([0,0,0,0], depth)
+    rewards = get_rewards_in_power_list(env, pl)
+    optimal_direction = np.argmax([get_max_from_power_list(rewards[i]) for i in range(4)])        
+    env.move(optimal_direction)
+    obstacles, mice, head, curr_direction, score, game_terminated = env.get_state()
+    hit_an_obstacle = game_terminated
+    
+    env.print_maze()
+
+# just to test
+state = env.get_state()
+obstacles, mice, head, curr_direction, score, game_terminated = state
+#print(obstacles)
+print(mice)
+print(head)
+print(curr_direction)
+
+print(size_of_accessible_region(state))
---
+++ environment_SE.py
@@ -0,0 +1,83 @@
+from collections import deque
+import random
+
+class environment:
+    
+    # mov[direction] is the displacement for the snake's head.
+    mov = [(0,1), (0,-1), (1,0), (-1,0)]
+    
+    """
+    Snake game on a square maze of size maze_size, snake with initial size of initial_snake_size.
+    Instead of a single mouse, there may be multiple mice. Their points are given by mice_points list.
+    """
+    def __init__(self, maze_size=15, initial_snake_size=5, mice_points=[1,2,3], fixed_obstacles=[(3,4),(3,5),(3,6)]):
+        self.game_terminated = False
+        self.score = 0
+        self.maze_size = maze_size
+        self.snake_q = deque()
+        # obstacles are walls + fixed obstacles + the snake itself
+        self.obstacles = set()
+        self.fixed_obstacles = fixed_obstacles
+        for obs in fixed_obstacles:
+            self.obstacles.add(obs)
+            # maze walls
+            for i in range (maze_size):
+                self.obstacles.add( (-1       ,i        ) );
+                self.obstacles.add( (maze_size,i        ) );
+                self.obstacles.add( (i        ,-1       ) );
+                self.obstacles.add( (i        ,maze_size) );
+        # snake
+        for i in range (initial_snake_size):
+            self.obstacles.add( (0,i) )
+            self.snake_q.append( (0,i) )
+        self.head = (0,initial_snake_size-1)
+        self.curr_direction = 0
+        # mice = dict{ key=coordinate, value=points }
+        self.mice = {}
+        for i in mice_points:
+            # takes care to not place a mouse on the snake or over another mouse
+            coord = (0,0)
+            while (coord in self.mice or coord in self.obstacles):
+                coord = (random.randint(0,maze_size), random.randint(0,maze_size))
+            self.mice[coord] = i
+        
+    def move(self, direction):
+        self.head = (self.head[0]+environment.mov[direction][0], self.head[1]+environment.mov[direction][1])
+        # checks if snake hits an obstacle
+        if self.head in self.obstacles:
+            self.game_terminated = True
+        else:
+            # adds new position
+            self.obstacles.add(self.head)
+            self.snake_q.append(self.head)
+            self.curr_direction = direction
+            # checks if ate a mouse
+            if self.head in self.mice:
+                points = self.mice[self.head]
+                self.score += points
+                # replace eaten mouse in a random position (without another mouse or the snake).
+                del self.mice[self.head]
+                coord = self.head
+                while (coord in self.mice or coord in self.obstacles):
+                    coord = (random.randint(0,self.maze_size), random.randint(0,self.maze_size))
+                self.mice[coord] = points 
+            else:
+                # remove tail
+                self.obstacles.remove(self.snake_q.popleft())
+        
+    def get_state(self):
+        return (self.obstacles, self.mice, self.head, self.curr_direction, self.score, self.game_terminated)
+
+    def print_maze(self):
+        print('score =', self.score)
+        for i in range(-1, self.maze_size + 1):
+            print('')
+            for j in range(-1, self.maze_size + 1):
+                if (i,j) in self.fixed_obstacles or i < 0 or i >= self.maze_size or j < 0 or j >= self.maze_size:
+                    print ('#', end='')
+                elif (i,j) in self.obstacles:
+                    print ('@', end='')
+                elif (i,j) in self.mice:
+                    print (self.mice[(i,j)], end='')
+                else:
+                    print (' ', end='')
