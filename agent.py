733ad601332cd44861e1842bee4977253965f654
Laurin Brandner <mail@laurinbrandner.ch>
2018-03-05 10:51:24 +0100

    Cleanup

--- agent.py
+++ agent.py
@@ -3,6 +3,10 @@ from environment import *
 from geometry import *
 
 
+def manhattan_distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
 def compute_direction(a, b):
     diff = b - a
     if abs(diff[0]) > abs(diff[1]):
@@ -17,7 +21,31 @@ def compute_direction(a, b):
             return 3 #left
 
 
-def decide(env, state):
+def decide1(env, state):
+    maze, head, tail, direction = state
+    maze_size = maze.shape[0]
+    
+    ds = [head + vectorize(direction, a) for a in actions]
+    ds = [1 if env.check_field(p) >= 0 else 0 for p in ds]
+    blocked_actions = int("".join(str(e) for e in ds), 2)
+    
+    mice = np.where(maze > 0)
+    mice = np.transpose(mice)
+    closest_mouse = np.argmin([manhattan_distance(head, m) for m in mice])
+
+    return (vec_to_int(head, maze_size), vec_to_int(mice[closest_mouse], maze_size), blocked_actions)
+
+
+def decide_dim1(maze_size):
+    maze_area = maze_size**2
+    n_c = 4 # direction of the mice
+    n_a = len(actions) #possible actions
+    n_d = 2**n_a #directions the snake can take without hitting an obstacle
+
+    return (maze_area, maze_area, n_d, n_a)
+
+
+def decide2(env, state):
     maze, head, tail, direction = state
     maze_size = maze.shape[0]
     
@@ -29,21 +57,23 @@ def decide(env, state):
     mice = np.transpose(mice)
     mice_directions = [compute_direction(head, m) for m in mice]
 
-    return (mice_directions[0], blocked_actions)
-
     return tuple(mice_directions + [blocked_actions])
+
+
+def decide_dim2():
+    n_c = 4 # direction of the mice
+    n_a = len(actions) #possible actions
+    n_d = 2**n_a #directions the snake can take without hitting an obstacle
+
+    return tuple(3*[n_c] + [n_d, n_a])
 
 
 def train_MC():
     epochs = 500000 # Number of episodes/plays
     epsilon = 1. # E-greedy
 
-    maze_size = 10
-    n_c = 4 # direction of the mice
-    n_a = len(actions) #possible actions
-    n_d = 2**n_a #directions the snake can take without hitting an obstacle
-
-    dim = tuple(1*[n_c] + [n_d, n_a])
+    maze_size = 15
+    dim = decide_dim1(maze_size)
     qv = np.zeros(dim) #This creates our Q-value look-up table
     sa_count = np.zeros(dim) #Record how many times we've seen a given state-action pair.
     returnSum = 0
@@ -58,13 +88,13 @@ def train_MC():
         ds = []
         
         while not ended:
-            d = decide(env, state)
+            d = decide1(env, state)
 
             # E-greedy policy
-            if (np.random.random() < epsilon or np.count_nonzero(qv[d[0], d[1],:]) == 0):
-                act = np.random.randint(0, n_a)
+            if (np.random.random() < epsilon or np.count_nonzero(qv[d[0], d[1], d[2],:]) == 0):
+                act = np.random.randint(0, len(actions))
             else:
-                act = np.argmax(qv[d[0], d[1],:]) #select the best action
+                act = np.argmax(qv[d[0], d[1], d[2],:]) #select the best action
 
             d = tuple(list(d) + [act]) #append the chosen action to the decision
 
@@ -91,15 +121,11 @@ def train_MC():
 def train_QL():
     epochs = 500000 # Number of episodes/plays
     epsilon = 1. # E-greedy
-    gamma = 1.0
+    gamma = 0.1
     alpha = 0.1
 
-    maze_size = 10
-    n_c = 4 # direction of the mice
-    n_a = len(actions) #possible actions
-    n_d = 2**n_a #directions the snake can take without hitting an obstacle
-
-    dim = tuple(1*[n_c] + [n_d, n_a])
+    maze_size = 15
+    dim = decide_dim1(maze_size)
     qv = np.zeros(dim) #This creates our Q-value look-up table
     returnSum = 0
     stepSum = 0
@@ -110,19 +136,19 @@ def train_QL():
         ended = False
         
         while not ended:
-            d = decide(env, state)
+            d = decide1(env, state)
 
             # E-greedy policy
-            if (np.random.random() < epsilon or np.count_nonzero(qv[d[0], d[1],:]) == 0):
-                act = np.random.randint(0, n_a)
+            if (np.random.random() < epsilon or np.count_nonzero(qv[d[0], d[1], d[2],:]) == 0):
+                act = np.random.randint(0, len(actions))
             else:
-                act = np.argmax(qv[d[0], d[1],:]) #select the best action
+                act = np.argmax(qv[d[0], d[1], d[2],:]) #select the best action
 
             d = tuple(list(d) + [act]) #append the chosen action to the decision
             
             state_new, reward, ended = env.step(act)
 
-            q_next = 0 if ended else np.max(qv[d[0], d[1],:])
+            q_next = 0 if ended else np.max(qv[d[0], d[1], d[2],:])
             qv[d] += alpha*(reward + gamma*q_next  -  qv[d])
             state = state_new
             returnSum += reward
@@ -134,5 +160,5 @@ def train_QL():
             returnSum = 0
             stepSum = 0
 
-
+        
 train_QL()
