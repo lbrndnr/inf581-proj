6f254ff709ef474d0b95f14cc64bb43fb16c65a8
Laurin Brandner <mail@laurinbrandner.ch>
2018-03-07 14:53:42 +0100

    Add results

--- results/plot.py
+++ results/plot.py
@@ -11,6 +11,6 @@ stats = np.delete(stats, n-1, 0)
 line1, = plt.plot(stats[:, 0], label="Reward")
 line2, = plt.plot(stats[:, 1], label="Time")
 plt.legend(handles=[line1, line2])
-plt.xlabel("Epochs")
+plt.xlabel("Epochs (in hundreds)")
 
 plt.show()
